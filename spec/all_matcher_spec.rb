e582fa11a4c38636f26d881539ea8679f2c191e9
Dongjae Heo <lindsay.heo@gmail.com>
2023-04-08 11:21:03 +0100

    all mathcer

---
+++ spec/all_matcher_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe 'all matcher' do
+  it 'allows for aggregate checks' do
+    expect([5, 7, 9]).to all(be_odd)
+    expect([2, 4, 6]).to all(be_even)
+    expect([0, 0]).to all(be_zero)
+    expect([5, 7, 9]).to all(be < 10)
+  end
+
+  describe [5, 7, 9] do
+    it { is_expected.to all(be < 10) }
+    it { is_expected.to all(be_odd) }
+  end
+end
